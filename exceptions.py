0efce4a0fddbc37079e65e747a37e540ed9320a4
Клавдия Дунаева <klavam@yandex.ru>
2022-11-02 20:30:34 +0700

    спринт 7, первый пошел

---
+++ exceptions.py
@@ -0,0 +1,16 @@
+
+class MassageDontSent(Exception):
+    pass
+
+
+class UrlNotAvailable(Exception):
+    pass
+
+
+class ApiAnswerError(Exception):
+    pass
+
+
+class TokenError(Exception):
+    pass
+
